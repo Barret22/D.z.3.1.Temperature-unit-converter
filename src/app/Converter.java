a095b7ac27c713ea2c4cae44e989adabb30b39de
Barret22 <vviktoroduvan@gmail.com>
2025-02-05 22:29:16 +0100

    Great class Converter

---
+++ src/app/Converter.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Converter {
+    public static void main(String[] args) {
+        System.out.println("Проект: Converter");
+        System.out.println("Версия: 1.0");
+        System.out.println("Автор: Виктор Клюев");
+    }
+}
