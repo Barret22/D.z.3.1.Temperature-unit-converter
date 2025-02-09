e58fe6472926a603780ae843774ee18b2402ebeb
Barret22 <vviktoroduvan@gmail.com>
2025-02-09 14:52:30 +0100

    Added conversion from Celsius to Fahrenheit

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -9,16 +9,32 @@ public class Converter {
         System.out.println("Автор: Виктор Клюев");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите температуру в" +
-                " Фаренгейтах: ");
-        double fahrenheit = scanner.nextDouble();
-        double celsius =
-                fahrenheitToCelsius(fahrenheit);
-        System.out.printf("Температура в " +
-                "Цельсиях: %.2f°C. ", celsius);
+        System.out.println("Выберите тип конвертации:");
+        System.out.println("1 - Фаренгейты в Цельсии");
+        System.out.println("2 - Цельсии в Фаренгейты");
+        System.out.print("Ваш выбор: ");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.print("Введите температуру в Фаренгейтах: ");
+            double fahrenheit = scanner.nextDouble();
+            double celsius = fahrenheitToCelsius(fahrenheit);
+            System.out.printf("Температура в Цельсиях: %.2f°C\n", celsius);
+        } else if (choice == 2) {
+            System.out.print("Введите температуру в Цельсиях: ");
+            double celsius = scanner.nextDouble();
+            double fahrenheit = celsiusToFahrenheit(celsius);
+            System.out.printf("Температура в Фаренгейтах: %.2f°F\n", fahrenheit);
+        } else {
+            System.out.println("Ошибка: Неверный выбор.");
+        }
     }
 
-    public static double fahrenheitToCelsius(double fahrenheit){
+    public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
